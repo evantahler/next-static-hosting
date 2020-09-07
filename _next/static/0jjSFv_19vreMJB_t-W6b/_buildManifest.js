a9172d32785eeed3551e770a4915319633837c6a
Deploy Robot <deploy@evantahler.com>
2020-09-07 16:49:16 +0000

    deploy static site @ Mon Sep  7 16:49:16 UTC 2020

--- _next/static/0jjSFv_19vreMJB_t-W6b/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {"/":["static\u002Fchunks\u002Fpages\u002Findex-6135c81a8335f340c64f.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-7f0375770732fe5d79ae.js"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
---
+++ _next/static/HzZPkEvVoagbGkH3vXo8M/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-8bf75d8b547dd42be7cd.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-4c0e59626af2fe2052a1.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
